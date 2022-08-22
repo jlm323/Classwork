d3b60962919372d143a95ece1e348df79feec728
Jessica Milewski <jlm323@pitt.edu>
2022-08-22 14:40:21 -0400

    finished section D change values

--- review/reviewingJS.js
+++ review/reviewingJS.js
@@ -1,5 +1,7 @@
 // VARIABLES AND DATATYPES
 
+// A. Q + A
+
 // A 1. We assign a value to a variable using the assignment operator (=)
 
 // A 2. If the variable has been declared with 'let', the variable can be reassigned so it has a new value. 
@@ -17,77 +19,82 @@
 // A 5. Around 80-90% of the time should be thinking about how to solve the problem versus actually coding it.
 
 
+// B. Strings
+
 // B 1. 
 // let firstVariable
 
 // B 2. 
-let firstVariable = 'Hello World'
+// let firstVariable = 'Hello World'
 
 // B 3. 
-firstVariable = 40
+// firstVariable = 40
 
 // B 4.
-let secondVariable = firstVariable
+// let secondVariable = firstVariable
 
 // B 5. 
-secondVariable = 'Goodbye'
+// secondVariable = 'Goodbye'
 
 // B 6. 
-console.log(firstVariable) // the value of firstVariable returns the value 40
+// console.log(firstVariable) // the value of firstVariable returns the value 40
 
 // B 7. 
-let yourName = "Jessica"
-console.log('Hello, my name is ' + yourName)
+// let yourName = "Jessica"
+// console.log('Hello, my name is ' + yourName)
 
 
 // C. Booleans
 
-const a = 4;
-const b = 53;
-const c = 57;
-const d = 16;
-const e = 'Kevin';
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
 
-console.log(a < b);
-console.log(c > d);
-console.log('Name' === 'Name');
-console.log(true || false);
-console.log(false && false && false && false && false || true);
-console.log(false == false);
-console.log(e === 'Kevin');
-console.log(a != b || c);
-console.log(a * a === d);
-console.log(48 == '48');
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name' === 'Name');
+// console.log(true || false);
+// console.log(false && false && false && false && false || true);
+// console.log(false == false);
+// console.log(e === 'Kevin');
+// console.log(a != b || c);
+// console.log(a * a === d);
+// console.log(48 == '48');
+
 
 // D. The Farm
 // D 1. 
-let animal = 'cow';
-animal = 'chicken';
+// let animal = 'cow';
+// animal = 'chicken';
 
-// D 2. 
-if (animal === 'cow') {
-    console.log('moooooo');
-} else {
-    console.log("Hey! You're not a cow.");
-}
+// // D 2. 
+// if (animal === 'cow') {
+//     console.log('moooooo');
+// } else {
+//     console.log("Hey! You're not a cow.");
+// }
+
 
 // E. Driver's Ed
 // E 1. 
-let giveKeys =
+// let giveKeys =
 
 // E 2.
-function(personAge) {
-    if (personAge > 16) {
-        console.log('Here are the keys!');
-    } else if (personAge < 16) {
-        console.log("Sorry, you're too young.");
-    }
-}
-giveKeys(40);
-giveKeys(12);
+// function(personAge) {
+//     if (personAge > 16) {
+//         console.log('Here are the keys!');
+//     } else if (personAge < 16) {
+//         console.log("Sorry, you're too young.");
+//     }
+// }
+// giveKeys(40);
+// giveKeys(12);
 
 // LOOPS
 
+// A. The basics
 // A 1.
 // for (let i = 0; i <= 10; i++) {
 //     console.log(i);
@@ -103,6 +110,8 @@ giveKeys(12);
 //     console.log(i);
 // };
 
+
+// B. Get even
 // B 1. and B 2.
 // for(let i = 1; i <= 100; i++) {
 //     if (i % 2 == 0) {
@@ -112,6 +121,8 @@ giveKeys(12);
 //     }
 // }
 
+
+// C. Give me five
 // C 1.
 // for (let i = 0; i <= 100; i++) {
 //     if (i % 5 == 0) {
@@ -123,3 +134,58 @@ giveKeys(12);
 //         console.log('I found a ' + i + '. High five!');
 //     }
 // }
+
+// D. SAVINGS ACCOUNT
+// D 1. 
+// let bank_account = 0;
+// let sum = 0;
+// for(let i = 1; i <= 10; i++) {
+//     bank_account = sum += i;
+// }
+
+
+// console.log(bank_account)
+
+
+// ARRAYS AND CONTROL FLOW
+
+// A. Talk about it
+// A 1. They are called elements
+
+// A 2. Yes, it is an ordered list. Unless the elements are changed, they will stay the same.
+
+// A 3. A grocery list, favorite things, etc.
+
+
+// B. Easy does it
+// B 1. 
+// let quotes = ['to be or not to be', 'go in the direction of your dreams', 'shall I compare thee'];
+
+// C. Accessing elements
+// C 1. 
+const randomThings = [1, 10, "Hello", true]
+// You would reference the index number of the first element which is [0].
+
+// C 2. 
+randomThings[2] = "World";
+
+// C 3. 
+console.log(randomThings)
+
+
+// D. Change values
+// D 1. 
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+// to access third element:
+ourClass[2];
+
+// D 2. 
+ourClass[4] = "Octocat";
+
+// D 3. 
+ourClass.push("Cloud City");
+
+console.log(ourClass)
+
+// E. Mix it up
+// E 1. 
