1acef10451e822b68c19b75d054fb0714b0772b4
Jessica Milewski <jlm323@pitt.edu>
2022-08-23 14:06:11 -0400

    Wild Wild Life answered

--- review/reviewingJS2.js
+++ review/reviewingJS2.js
@@ -18,14 +18,38 @@
 
 // Fizz Buzz
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('Fizz Buzz');
-    } else if (i % 3 === 0) {
-        console.log('Fizz');
-    } else if (i % 5 === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('Fizz Buzz');
+//     } else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+
+// Wild Wild Life
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ['Sharky', 'shark', 20, 'Left Coast']
+const plantee = ['Plantee', 'plant', 5000, 'Mordor']
+const porgee = ['Porgee', 'Porg', 186, 'Ahch-To']
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]++;
+console.log(plantee);   // one year older
+
+wolfy[3] = 'Gotham City';
+console.log(wolfy);     // change hometown
+
+dart.push("Hawkins");
+console.log(dart);
+
+wolfy.shift();
+wolfy.unshift('Gameboy');
+console.log(wolfy);
+
