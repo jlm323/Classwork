2b2cf570479b2af69a0251ae19990e7cdb48e893
Jessica Milewski <jlm323@pitt.edu>
2022-08-23 14:40:39 -0400

    Methods revisited answered

--- review/reviewingJS2.js
+++ review/reviewingJS2.js
@@ -57,8 +57,44 @@
 
 // Yell at the Ninja Turtles
 
-const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+// const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 
-for (let turtles of ninjaTurtles) {
-    console.log(turtles.toUpperCase());
-}
+// for (let turtles of ninjaTurtles) {
+//     console.log(turtles.toUpperCase());
+// }
+
+
+
+// Methods, revisited
+
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
+
+// find index of Titanic
+let findTitanic = i => i === 'Titanic';
+console.log(favMovies.findIndex(findTitanic));
+
+// favMovies.sort();
+// console.log(favMovies);     // sorts strings into alphabetical order
+
+favMovies.pop();
+favMovies.push('Guardians of the Galaxy');
+
+favMovies.reverse();
+
+favMovies.shift();
+
+favMovies.unshift('Princess and the Pauper'); // doesn't return anything unless something is in the parentheses
+
+let findDjango = i => i === 'Django Unchained';
+console.log(favMovies.findIndex(findDjango));   // index is 15
+
+favMovies.splice(15, 1, 'Avatar');
+
+// console.log(favMovies.length); // output: 19
+
+console.log(favMovies.slice(10));
+
+let findFast = i => i === 'Fast and Furious';
+console.log(favMovies.findIndex(findFast)); // output is -1 because it is not in the array
+
+// if you want the array to be mutated, declare it with const. if you want the array to get reassigned, declare it with let.
