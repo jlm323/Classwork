d5957746618eafebea10f584c28f6c9d37b802bf
Jessica Milewski <jlm323@pitt.edu>
2022-08-23 13:51:46 -0400

    Get Even answered

--- review/reviewingJS2.js
+++ review/reviewingJS2.js
@@ -2,6 +2,11 @@
 
 // Easy going
 
-for(let i = 1; i <= 20; i++) {
+// for(let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+// Get Even
+for(let i = 0; i <= 200; i+=2) {
     console.log(i);
 }
