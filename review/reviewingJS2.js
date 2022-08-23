93090098831095c98397a705c87857891ade57b0
Jessica Milewski <jlm323@pitt.edu>
2022-08-23 13:56:24 -0400

    Fizz Buzz answered

--- review/reviewingJS2.js
+++ review/reviewingJS2.js
@@ -6,7 +6,26 @@
 //     console.log(i);
 // }
 
+
+
 // Get Even
-for(let i = 0; i <= 200; i+=2) {
-    console.log(i);
+
+// for(let i = 0; i <= 200; i+=2) {
+//     console.log(i);
+// }
+
+
+
+// Fizz Buzz
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz Buzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 }
