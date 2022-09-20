1f51e481d2feaa732b19e963f1ecb26b897d9b29
Jessica Milewski <jlm323@pitt.edu>
2022-09-20 17:40:45 -0400

    node lab

---
+++ node-practice/practice-exercises/utilities/script.js
@@ -0,0 +1,9 @@
+// let random = require('./random')
+// for (let i = 0; i < 10; i++) {
+//     console.log( random(100, 200) );
+// }
+
+
+const circle = require('./circle');
+  console.log( circle.area(50) ); 
+  console.log( circle.circumference(75) );
